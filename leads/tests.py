e69303f915b20a1a91f31e23f2debfe27d84b615
Paul Rogers <paulrogers909@protonmail.com>
2019-04-06 18:14:03 +0100

    add src

---
+++ leads/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import Lead
+
+
+class LeadModelTest(TestCase):
+    def test_str_representation(self):
+        lead = Lead.objects.create(
+            name='Jenny',
+            email='jenny@mail.com',
+            message='New film role please'
+        )
+        self.assertEqual(str(lead), 'jenny@mail.com')
