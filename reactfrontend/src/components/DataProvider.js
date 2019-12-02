a7063a9d1d2d13ae47a2ecba0255f21e9d07c893
Paul Rogers <paulrogers909@protonmail.com>
2019-12-02 15:19:28 +0000

    mimic slow network

--- reactfrontend/src/components/DataProvider.js
+++ reactfrontend/src/components/DataProvider.js
@@ -6,21 +6,24 @@ const DataProvider = props => {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchData = () => {
-      setIsLoading(true);
-      fetch(props.endpoint)
-        .then(res => {
-          if (res.status !== 200) {
-            return { message: "Error connecting" };
-          }
-          return res.json();
-        })
-        .then(data => setData(data))
-        .catch(() => {
-          console.error();
-        });
-    };
-    fetchData();
+    // mimic slow network 2000ms
+    setTimeout(() => {
+      const fetchData = () => {
+        setIsLoading(true);
+        fetch(props.endpoint)
+          .then(res => {
+            if (res.status !== 200) {
+              return { message: "Error connecting" };
+            }
+            return res.json();
+          })
+          .then(data => setData(data))
+          .catch(() => {
+            console.error();
+          });
+      };
+      fetchData();
+    }, 2000);
   }, []);
 
   return isLoading ? (
